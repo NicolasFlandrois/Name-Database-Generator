af563d224b677f730d6b45bb670a807f67682544
NicolasFlandrois <n.flandrois9github@gmail.com>
2019-05-27 17:17:39 +0200

    Testing issues and questions summary
    Stuffs to work on, solve, and clean up for French Social Sec number generator. Also need to use INSEE's database for Foreign counties identifier, and city of birth ID code.

--- social_sec_gen.py
+++ social_sec_gen.py
@@ -4,10 +4,12 @@
 # Description: Script creating a French social security number
 from random import randrange, choice
 
-#First digit: sex/gender
+#First digit: sex/gender 1==M | 2==F
 sex = randrange(1, 3)
 
 #Second 2 digits: Year of birth
+#How to manage the difference between XXth and XXIst century, years? 
+#19xx vs 20xx, How to determine that a year expl: 17 stands for 1917 or 2017?
 birth_year = randrange(0,100)
 
 #Third 2 digits: Month of birth
@@ -52,6 +54,38 @@ control_key = int(97-(ss_int-((round((ss_int/97), 0))*97)))
 
 ss_str = '{} {:02d} {:02d} {:02d} {:03d} {:03d} - {:02d}'.format(
 	sex, birth_year, birth_month, birth_dep, birth_city, birth_id, control_key)
+
 print('auto: ', ss_str)
+
 #print('manual verification: ',
 # sex, birth_year, birth_month, birth_dep, birth_city, birth_id, control_key)
+
+# Description du NIR
+# Le numéro NIR (Numéro d'Inscription au Répertoire) est le numéro d'inscription
+# au répertoire national d'identification des personnes physiques, 
+#et il est identique au numéro de sécurité sociale. La gestion du numéro NIR a 
+#été confiée à l'INSEE, c'est pourquoi on parle parfois aussi du numéro INSEE.
+
+# Ce numéro sert d'identifant unique pour chaque individu inscrit au RNIPP 
+#(Répertoire National d'Identification des Personnes Physiques).
+
+# La clé du numéro NIR est constituée de 2 chiffres et est comprise entre 01 et 
+#97. Elle permet de vérifier la validité des 13 chiffres du numéro, grâce à un 
+#algorithme qui lui est appliqué.
+
+# Algorithme de calcul de la clé du numéro
+# La clé NIR est calculée par une formule mathématique, qui ne peut s'appliquer 
+#que sur des valeurs numériques. Il convient donc de remplacer les lettres 
+#présentes dans les départements corses :
+# 2A va devenir 19 et 2B devient 18.
+
+# La clé peut alors être calculée avec la formule suivante :
+
+# Clé NIR = 97 - ( ( Valeur numérique du NIR ) modulo 97 )
+
+#NB: 
+#Not quite there yet. The script I created still gives a few numbers with a 
+#control key of 3 digits, instead of 2. 
+#e.g auto:  1 06 05 84 831 835 - 124 >> Control key should be 27 instead of 124.
+#And misslead on how to manage Corsica numbers as they contain letters, 
+#2A and 2B. See description above.
